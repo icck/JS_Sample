da4cc460d2c2a1d2396cfd078a53ac6ebf1366e4
icck <icck.n.n1111@gmail.com>
2019-07-21 02:44:43 +0900

    add method

---
+++ method.js
@@ -0,0 +1,14 @@
+let createMl = () => {
+    console.log('hello ml');
+};
+
+createMl();
+
+let createarg = (arg) => {
+    console.log(String(arg) + 'output')
+};
+
+createarg();
+createarg(1234);
+createarg('5678');
+createarg('test');
