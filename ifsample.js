ad12c45469bc49dab12e477c617195f547d999c1
icck <icck.n.n1111@gmail.com>
2019-07-20 17:19:33 +0900

    if else else if

--- ifsample.js
+++ ifsample.js
@@ -10,3 +10,19 @@ let value = '80';
 if (! isNaN(value)) {
     console.log(parseInt(value)+80);
 }
+
+let el = '80';
+if (! isNaN(el)) {
+    console.log('number');
+}else {
+    console.log('not number');
+}
+
+let age = 9;
+if (age > 10) {
+    console.log('1 9');
+} else if(age > 5){
+    console.log('2');
+}else{
+    console.log('3');
+}
